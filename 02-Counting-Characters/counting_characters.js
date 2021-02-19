b679f553c0d7b54b5ad954e918c9ec6d09f2354a
Siar Sohrab <siar.sohrab@gmail.com>
2021-02-19 14:28:31 +0100

    added second excrcise

--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -2,7 +2,17 @@
 /*
 
 here
+1-Define variable that take a text as an input and assign it to "write everything that you want"
+2- define another variable called name
+3- define another veriable called lenght this variable will mesure and check how many characters in the text
+4- now we print the text and the lenght of the text.
 
 */
 
 //insert your code below
+var text, length2;
+
+
+text = window.prompt('Write everything that you want');
+length2 = text.length;
+window.alert([text,',',length2].join(''));
