d0a2c063af71d700e721801ab6541b65cb2a7cb9
Siar Sohrab <siar.sohrab@gmail.com>
2021-02-19 12:47:30 +0100

    added ex 1

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -2,7 +2,11 @@
 /*
 
 here
-
+Define and input form and assign it to a text to include the requested name
+Define a varible that take a tezt as an input and assign it to "write your name"
+our variable clled Name
+Define a tex that says the greeting sentence you want for my case "welcom"
+print welcome + the varaible "Name" 
 */
 
 //insert your code below
